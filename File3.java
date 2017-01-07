abd8cb9b33b8e797e8c44bea5e44f5b2fd796cdc
scott-summers <2015470098@ftumj.ac.id>
2017-01-07 21:01:40 +0000

    Create File3.java

---
+++ File3.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class File3 {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Masukkan beberapa bilangan: "); // misalkan 2 1 4 2 (pisahkan dengan spasi)
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		sc.close();
+		
+		String[] s = input.split(" "); // memisah-misahkan string "input" ke dlm array "s" dengan 'spasi' sbg pemisah
+		
+		// Konversi String ke integer
+		int[] a = new int[s.length];
+		for (int i=0; i<a.length; i++) {
+			a[i] = Integer.parseInt(s[i]); // konversi String ke integer (dari array s[] ke a[])
+		}
+		
+		// Insertion Sort
+		for (int i=1; i < a.length; i++) {
+	        int copyArray = a[i];
+	        int j = i;
+	        while (j > 0 && copyArray < a[j-1]) {
+	        	a[j] = a[j-1];
+	            j--;
+	        }
+	        a[j] = copyArray;
+		}
+
+    // menampilkan bilangan yg sudah diurutkan
+		System.out.print("Bilangan setelah diurutkan: ");
+		for (int i=0; i<a.length; i++) {
+			System.out.print(a[i]+" ");
+		}
+		
+		// Median
+		if (a.length%2==1) { //Median Ganjil
+			int x = a.length/2;
+			System.out.println("\nMed.Gj: "+a[x]);
+		}
+		else { //Median Genap
+			float x1 = (a[a.length/2]);
+			float x2 = (a[(a.length/2)-1]);
+			float x = (x1+x2)/2;
+			System.out.println("\nMed.Gp: "+x);
+		}
+
+	}
+
+}
