df2717dc5bd995eece9e93bf0a2aa5f8fdbacef0
scott-summers <2015470098@ftumj.ac.id>
2017-01-07 21:14:08 +0000

    Update File3.java

--- File3.java
+++ File3.java
@@ -17,37 +17,37 @@ public class File3 {
 		int[] a = new int[s.length];
 		for (int i=0; i<a.length; i++) {
 			a[i] = Integer.parseInt(s[i]); // konversi String ke integer (dari array s[] ke a[])
-		}
+		} //end for
 		
 		// Insertion Sort
 		for (int i=1; i < a.length; i++) {
-	        int copyArray = a[i];
-	        int j = i;
-	        while (j > 0 && copyArray < a[j-1]) {
-	        	a[j] = a[j-1];
-	            j--;
-	        }
-	        a[j] = copyArray;
-		}
+			int copyArray = a[i];
+			int j = i;
+			while (j > 0 && copyArray < a[j-1]) {
+				a[j] = a[j-1];
+				j--;
+			} //end while
+	        	a[j] = copyArray;
+		} //end for
 
    		 // menampilkan bilangan yg sudah diurutkan
 		System.out.print("Bilangan setelah diurutkan: ");
 		for (int i=0; i<a.length; i++) {
 			System.out.print(a[i]+" ");
-		}
+		} //end for
 		
 		// Median
 		if (a.length%2==1) { //Median Ganjil
 			int x = a.length/2;
 			System.out.println("\nMed.Gj: "+a[x]);
-		}
+		} // end if
 		else { //Median Genap
 			float x1 = (a[a.length/2]);
 			float x2 = (a[(a.length/2)-1]);
 			float x = (x1+x2)/2;
 			System.out.println("\nMed.Gp: "+x);
-		}
+		} // end else
 
-	}
+	} // end main
 
-}
+} // end class
